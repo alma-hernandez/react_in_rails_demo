cf3e1320c772c37597f5e9c345e1a67db024c16f
alma-hernandez <1984ahern@gmail.com>
2024-03-12 10:40:11 -0500

    3.10

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './layout';
+import "./home.scss";
+
+
 const Home = () => (
   <Layout>
-    <h1>Home page</h1>
+    <h1>Home page webpack dev server working</h1>
   </Layout>
 )
 document.addEventListener('DOMContentLoaded', () => {
