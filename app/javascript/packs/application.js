1bbac339f8c013483f5f11db5b50128d7d407363
alma-hernandez <1984ahern@gmail.com>
2024-03-12 12:02:58 -0500

    finished demo 3.14

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,22 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap";
+import { safeCredentials, handleErrors } from './utils/fetchHelper';
+// change the path to fetchHelper.js to the one that fits your file structure
+
+fetch(`/users`, safeCredentials({
+  method: 'POST',
+  body: JSON.stringify({
+    users: {
+      email: 'hello@world.com',
+      password: 'abc123'
+    }
+  })
+}))
+.then(handleErrors)
+.then(res => {
+  console.log(res);
+})
 
 Rails.start()
 //Turbolinks.start()
